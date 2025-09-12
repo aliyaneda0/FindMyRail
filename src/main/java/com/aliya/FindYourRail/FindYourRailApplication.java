f596621832c3eec854523eb6ed91bcad67ea8738
aliya neda <nedaaliya4@gmail.com>
2025-09-12 22:50:30 +0530

    First Commit

---
+++ src/main/java/com/aliya/FindYourRail/FindYourRailApplication.java
@@ -0,0 +1,13 @@
+package com.aliya.FindYourRail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourRailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourRailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliya/FindYourRail/FindYourRailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliya.FindYourRail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourRailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
